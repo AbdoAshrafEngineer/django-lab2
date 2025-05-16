4e5793eae9c3b7a3a58f39fb99b81b14c4ca8079
Abdelrahman <abdelrahman.ashraf.engineer@gmail.com>
2025-05-16 15:49:25 +0300

    create category class model

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True, max_length=50)
+    desc = models.TextField(null=True, blank=True)
+
+
+
