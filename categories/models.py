50ee5e8c114261b3c50dc4ed3b77bd6cbd772950
Abdelrahman <abdelrahman.ashraf.engineer@gmail.com>
2025-05-16 17:39:38 +0300

    added screenshot from postgres dbms

--- categories/models.py
+++ categories/models.py
@@ -7,6 +7,3 @@ class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(unique=True, max_length=50)
     desc = models.TextField(null=True, blank=True)
-
-
-
